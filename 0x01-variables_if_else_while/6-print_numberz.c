ed0583a2cd7c7e1ce68c5b622fc0329b458f564c
PeterAddae <addaepeter41@gmail.com>
2022-09-09 13:14:15 -0700

    six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Alwys 0 (Success/correct)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar (i);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
