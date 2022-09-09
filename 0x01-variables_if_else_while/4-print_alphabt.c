3e5b7911724196997511d1807ba15809ce4f9d26
PeterAddae <addaepeter41@gmail.com>
2022-09-09 10:45:39 -0700

    mm

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Sucess/correction)
+ */
+int main(void)
+{
+char alphabeth;
+
+for (alphabeth = 'a'; alphabeth <= 'z'; alphabeth++)
+{
+	if (alphabeht == 'q')
+		continue;
+
+	else if (alphabeth == 'e')
+		continue;
+
+	putchar(alphabeth);
+}
+
+putchar ('\n');
+
+return (0);
+}
