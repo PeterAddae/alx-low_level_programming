3555212481f8a02aaba460ae7e8af0a5c4ebfd2e
PeterAddae <110790556+PeterAddae@users.noreply.github.com>
2022-09-14 05:13:46 +0000

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c: a character to be checked
  * Return: returns 0 or 1 depending on conditions
  */
-int _isalpha(int c) 
+int _isalpha(int c)
 {
-    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
