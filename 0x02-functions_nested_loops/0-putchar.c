ef6d201220cd92d3ee83f10d76565853d1845e9e
PeterAddae <110790556+PeterAddae@users.noreply.github.com>
2022-09-14 03:59:17 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /** 
- * main - prints putchar
- * Return: Always 0
- */
+* main - prints putchar
+* Return: Always 0
+*/
 int main(void)
 {
     _putchar('_');
@@ -16,5 +16,5 @@ int main(void)
     _putchar('r');
     _putchar('\n');
 
-    return (0);
+ return (0);
 }
