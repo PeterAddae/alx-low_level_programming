acb5b9b322e262872fa27b27b3e9f8c7935b5b2f
PeterAddae <110790556+PeterAddae@users.noreply.github.com>
2022-09-15 11:38:10 +0000

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -29,4 +29,7 @@ printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 else
 {
 printf("Day of the year: %d\n", day);
-printf("Remaning days: %d\n", 365 - day); } } }
+printf("Remaning days: %d\n", 365 - day); 
+}
+} 
+}
