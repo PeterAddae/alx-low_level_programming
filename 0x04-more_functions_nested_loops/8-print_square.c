6f94fdee54dba0c2b50335ac2c57901e263cffd9
PeterAddae <110790556+PeterAddae@users.noreply.github.com>
2022-09-16 00:21:03 +0000

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,11 +2,9 @@
 #include "main.h"
 
 /**
-* main - prints the numbers from 1 to 100, followed by a new line
-* but for multiples of three prints Fizz instead of the number
-* and for the multiples of five prints Buzz
+* main - prints the numbers from 1 to 100, followed by a new line but for multiples of three prints Fizz instead of the number and for the multiples of five prints Buzz
 * Return: Always 0 (Success)
-*/ 
+*/
 int main(void)
 {
 int i;
