8cf04e523fd05258c48727f40f0bd38deb0147a5
PeterAddae <110790556+PeterAddae@users.noreply.github.com>
2022-09-21 08:17:28 +0000

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,7 +8,7 @@
  * @n: integer parameter to compare index
  * Return: returns new concatenated string
  */
-char *strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 int index = 0, dest_len = 0;
 
