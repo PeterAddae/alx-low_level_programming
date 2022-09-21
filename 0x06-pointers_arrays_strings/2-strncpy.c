ceb9e3bbbfc09307ac813cbe6b07afb04bf48521
PeterAddae <addaepeter41@gmail.com>
2022-09-21 08:27:12 +0000

    asda

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
-for (i = 0; i < n && src[i] =! '\0'; i++)
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
 dest[i] = src[i];
 }
@@ -20,5 +20,4 @@ for (; i < n; i++)
 {
 dest[i] = '\0';
 }
-return (dest);
-}
+return (dest); }
