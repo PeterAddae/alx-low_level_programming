e97bf3ff216bf5c1c103cb0d07074da174f7bd65
PeterAddae <110790556+PeterAddae@users.noreply.github.com>
2022-10-05 11:37:36 +0000

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,7 +12,7 @@ char *_strcat(char *dest, char *src)
 int a, b;
 
 for (a = 0; dest[a] != '\0'; a++)
-                ;
+;
 for (b = 0; src[b] != '\0'; b++)
 {
 dest[a] = src[b];
@@ -32,7 +32,8 @@ int _strlen(char *s)
 {
 int a;
 
-for (a = 0; s[a] != '\0'; a++);
+for (a = 0; s[a] != '\0'; a++)
+;
 return (a);
 }
 
