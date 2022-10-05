dcb72bd65ed9b9d48b2d3a499fa2b36ec4f341e1
PeterAddae <110790556+PeterAddae@users.noreply.github.com>
2022-10-05 11:09:49 +0000

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,15 +14,15 @@
 
 char *create_array(unsigned int size, char c)
 {
-	unsigned int a;
-	char *s;
+unsigned int a;
+char *s;
   
-	s = (char *) malloc(size * sizeof(char));
-	if (s == NULL)
-		return (NULL);
-	for (a = 0; a < size; a++)
-	{
-		s[a] = c;
-	}
-	return (s);
+s = (char *) malloc(size * sizeof(char));
+if (s == NULL)
+return (NULL);
+for (a = 0; a < size; a++)
+{
+s[a] = c;
+}
+return (s);
 }
